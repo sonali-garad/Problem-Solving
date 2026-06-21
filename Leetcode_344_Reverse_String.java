c98e570ff9e0a8ca0a0efcdac76d4039ecc0bee0
sonali garad <sonaligarad1234@gmail.com>
2026-06-21 22:38:06 +0530

    string - sol

--- Leetcode_344_Reverse_String.java
+++ Leetcode_344_Reverse_String.java
@@ -17,7 +17,37 @@
 
 // 1 <= s.length <= 105
 // s[i] is a printable ascii character.
+import java.util.*;
+
 public class Leetcode_344_Reverse_String {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        char s[];
+        System.out.print("How many char want in s array : ");
+        n = sc.nextInt();
+        s = new char[n];
+        System.out.print("Enter char for s array : ");
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next().charAt(0);
+        }
+        solution(s);
+    }
+
+    public static void solution(char[] s) {
+        int right = s.length - 1;
+        // s o n a l i ;
+        for (int left = 0; left < right; left++) {
+            char start = s[left];
+            char end = s[right];
+            char temp = ' ';
+            temp = start;
+            s[left] = end;
+            s[right] = temp;
+            right--;
+
+        }
+        System.out.print("Output : " + Arrays.toString(s));
+
     }
 }
