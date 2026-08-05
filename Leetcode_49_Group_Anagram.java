c42650f4d3ce915bd6e5656409a42bfa7c8532cc
sonali garad <sonaligarad1234@gmail.com>
2026-08-05 23:13:55 +0530

    medium->49

--- Leetcode_49_Group_Anagram.java
+++ Leetcode_49_Group_Anagram.java
@@ -1,54 +1,75 @@
+//LEETCODE - 49 - GROUP ANAGRAM 
+
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Explanation:
+
+// There is no string in strs that can be rearranged to form "bat".
+// The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+// The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+// Example 2:
+
+// Input: strs = [""]
+
+// Output: [[""]]
+
+// Example 3:
+
+// Input: strs = ["a"]
+
+// Output: [["a"]]
+
+// Constraints:
+
+// 1 <= strs.length <= 104
+// 0 <= strs[i].length <= 100
+// strs[i] consists of lowercase English letters.
+
 import java.util.*;
+
 public class Leetcode_49_Group_Anagram {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
         String str[];
-        int n ; 
+        int n;
         System.out.print("How many string do u want in str array :");
-        n = sc.nextInt(); 
+        n = sc.nextInt();
+        sc.nextLine();
         str = new String[n];
-        for (int i = 0; i<n ; i++)
-        {
+        for (int i = 0; i < n; i++) {
             str[i] = sc.nextLine();
         }
         List<List<String>> res = solution(str);
-        System.out.println("output : "+res);
+        System.out.println("output : " + res);
 
     }
-    public static List<List<String>> solution (String[] str)
-    {
-        List<List<String>> list = new ArrayList<List<String>>() ;
-        List<String> list1 = new ArrayList<>();
-  
-        for (int i = 0 ; i<str.length ; i++)
-        {
-            char array[] = str[i].toCharArray();
-            int len = array.length;
-           boolean flag = false ;
-            for(int j= i+1 ; j<str.length ; j++)
-            {
-             char compare[] = str[j].toCharArray();
-             int idx = 0 ; 
-             int comp= 0 ;
-              while (len > 0) {
-                if(array[idx] == compare[comp] && comp < compare.length)
-                {
-                    flag = true ;
-                }
-                idx--;
-                comp++;
-              }  
-               if (flag)
-            {
-                list1.add(str[j]);
-             
-            } 
+
+    public static List<List<String>> solution(String[] str) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < str.length; i++) {
+            String s = str[i];
+            char c[] = s.toCharArray();
+            Arrays.sort(c);
+            String comapre = new String(c); // ant
+            // check is key allready exits
+            if (map.containsKey(comapre)) {
+                map.get(comapre).add(str[i]);
+            } else {
+                List<String> list1 = new ArrayList<>();
+                list1.add(s);
+                map.put(comapre, list1);
+
             }
-            list.add(list1);
 
-             //[res, ser , tan, nat ]
+        }
+        System.out.println(map);
+
+        return new ArrayList<>(map.values());
     }
-    System.out.println(list);
-    return null;
-}
 }
