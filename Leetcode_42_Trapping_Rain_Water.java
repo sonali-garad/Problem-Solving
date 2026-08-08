93cd99a956c2a8b23bd3dff923a95b1477d67730
sonali garad <sonaligarad1234@gmail.com>
2026-08-09 01:43:47 +0530

    better approach

--- Leetcode_42_Trapping_Rain_Water.java
+++ Leetcode_42_Trapping_Rain_Water.java
@@ -37,39 +37,93 @@ public class Leetcode_42_Trapping_Rain_Water {
 
     public static int solution(int height[]) {
         // brute force - o(n^2)
+        // int water = 0;
+        // for (int i = 1; i < height.length - 1; i++) {
+        // int left = Integer.MIN_VALUE;
+        // int right = Integer.MIN_VALUE;
+        // int idx = 0;
+        // int rightidx = i + 1;
+        // // 3 5 4 1 7 6
+        // int current = height[i];
+
+        // while (idx < i) {
+        // if (height[idx] > left) {
+        // left = height[idx];
+        // }
+        // idx++;
+
+        // }
+        // while (rightidx < height.length) {
+        // if (height[rightidx] > right) {
+
+        // right = height[rightidx];
+        // }
+        // rightidx++;
+
+        // }
+
+        // int value = Math.min(right, left);
+        // if (current <= value) {
+        // water = value - current + water;
+
+        // System.out.println("water : " + water);
+        // }
+        // }
+
+        // return water;
+
+        // Better soln : time o(n) and space o(n)
+        int leftmax[] = new int[height.length];
+        int rightmax[] = new int[height.length];
         int water = 0;
-        for (int i = 1; i < height.length - 1; i++) {
-            int left = Integer.MIN_VALUE;
-            int right = Integer.MIN_VALUE;
-            int idx = 0;
-            int rightidx = i + 1;
-            // 3 5 4 1 7 6
-            int current = height[i];
+        // 2 3 5 7 4 2 8
+        leftmax[0] = height[0];
+        rightmax[height.length - 1] = height[height.length - 1];
+        for (int i = 1; i < height.length; i++) {
+            // int left = i - 1;
 
-            while (idx < i) {
-                if (height[idx] > left) {
-                    left = height[idx];
-                }
-                idx++;
+            // int idx = 0;
+            // while (i > idx) {
+            // if (height[idx] > left)
+            // {
+            // left = height[idx];
+            // leftmax[i] = left;
+            // }
+            // idx++;
+            // }
 
-            }
-            while (rightidx < height.length) {
-                if (height[rightidx] > right) {
-
-                    right = height[rightidx];
-                }
-                rightidx++;
-
+            if (height[i - 1] >= leftmax[i - 1]) {
+                leftmax[i] = height[i - 1];
+            } else {
+                leftmax[i] = leftmax[i - 1];
             }
 
-            int value = Math.min(right, left);
-            if (current <= value) {
-                water = value - current + water;
-
-                System.out.println("water : " + water);
-            }
         }
 
+        for (int i = height.length - 2; i >= 0; i--) {
+            // int right = Integer.MIN_VALUE;
+            // 3 2 5 4 3
+            // for (int j = i+1 ; j< height.length ; j++)
+            // {
+            // if (height[j] > right)
+            // {
+            // right = height[j];
+            // rightmax[i] = right;
+            // }
+            // }
+            rightmax[i] = Math.max(height[i + 1], rightmax[i + 1]);
+
+        }
+        System.out.println(Arrays.toString(leftmax));
+        System.out.println(Arrays.toString(rightmax));
+
+        for (int i = 1; i < height.length - 1; i++) {
+            int value = Math.min(leftmax[i], rightmax[i]);
+            if (height[i] <= value) {
+                water = water + value - height[i];
+            }
+        }
         return water;
+
     }
 }
