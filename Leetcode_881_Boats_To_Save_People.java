bc927d221657a4f24ee2beae64bc1eb4f1f3bcd3
sonali garad <sonaligarad1234@gmail.com>
2026-08-11 22:46:59 +0530

    881-medium

---
+++ Leetcode_881_Boats_To_Save_People.java
@@ -0,0 +1,69 @@
+// LEETCODE - 881 - BOATS TO SAVE PEOPLE 
+
+// You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+// Return the minimum number of boats to carry every given person.
+
+// Example 1:
+
+// Input: people = [1,2], limit = 3
+// Output: 1
+// Explanation: 1 boat (1, 2)
+// Example 2:
+
+// Input: people = [3,2,2,1], limit = 3
+// Output: 3
+// Explanation: 3 boats (1, 2), (2) and (3)
+// Example 3:
+
+// Input: people = [3,5,3,4], limit = 5
+// Output: 4
+// Explanation: 4 boats (3), (3), (4), (5)
+
+// Constraints:
+
+// 1 <= people.length <= 5 * 104
+// 1 <= people[i] <= limit <= 3 * 104
+
+import java.util.*;
+
+public class Leetcode_881_Boats_To_Save_People {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n, people[], limit;
+        System.out.print("How many numbers do u want in people array: ");
+        n = sc.nextInt();
+        people = new int[n];
+        for (int i = 0; i < n; i++) {
+            people[i] = sc.nextInt();
+        }
+        System.out.print("Enter limit : ");
+        limit = sc.nextInt();
+        int res = solution(people, limit);
+        System.out.println("output : " + res);
+    }
+
+    public static int solution(int[] people, int limit) {
+
+        int left = 0;
+        int right = people.length - 1;
+        int count = 0;
+
+        Arrays.sort(people);
+
+        while (left <= right) {
+
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
